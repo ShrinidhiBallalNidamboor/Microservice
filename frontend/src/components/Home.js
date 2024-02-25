e5aaf0c7b1f829db8c57f37d8be1aedbb26cf27e
Abhishek S <90543211+AbhishekS8073@users.noreply.github.com>
2024-02-25 19:28:35 +0530

    Added React Components

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Home = () => {
+    return (
+        <div><Navbar active="projects"></Navbar>
+        <div>Hello</div>
+        </div>
+        
+    )
+}
+
+export default Home;
