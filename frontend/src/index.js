20c04daf0dfa2af0a785d862bd51b64eb5b8d2d5
Abhishek S <90543211+AbhishekS8073@users.noreply.github.com>
2024-03-21 15:24:23 +0530

    Added index.js\

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import './index.css';
 import Home from './components/Home';
+import Qna from './components/Qna';
+import Trail from './components/Trail';
+import Answers from './components/Answers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/Qna' element={<Qna />}></Route>
+      <Route path='/Answers' element={<Answers />}></Route>
+      <Route path='/Trail' element={<Trail />}></Route>
       </Routes>
     </BrowserRouter>
 
